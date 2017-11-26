005c36bba470fdcd27c6157ceeeba5e629004d50
tian <tian@tian.workgroup>
2017-11-26 16:50:38 +0800

    “”

---
+++ config/server.js
@@ -0,0 +1,14 @@
+'use strict';
+
+module.exports = {
+    redisOption: {
+        host: 'localhost',
+        port: '6379'
+    },
+    mysqlOption: {
+        host: 'localhost',
+        user: 'root',
+        password: 'haiyouwo',
+        database: 'music_server'
+    }
+};
